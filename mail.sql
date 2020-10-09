72b4d5302ac79d499ab95fcb5fd80f0f03c7dfef
tenitski <alexei@te.nz>
2020-10-09 19:49:23 +1300

    Updated Postfix config to use new db schema

--- mail.sql
+++ mail.sql
@@ -1,42 +1,9 @@
 CREATE DATABASE mail;
-USE DATABASE mail;
 
 GRANT ALL ON mail.* TO 'mail'@'localhost' IDENTIFIED BY 'mail';
 FLUSH PRIVILEGES;
 
-CREATE USER 'mail'@'localhost' IDENTIFIED BY 'mail';
-GRANT ALL ON mail.* TO 'mail'@'localhost';
-
-CREATE TABLE users (
-    user varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    PRIMARY KEY (user)
-);
-
-INSERT INTO users (user, password) VALUES
-    ("mailer@example.com", "password1"),
-    ("mailer@example.net", "password2"),
-    ("mailer@example.org", "password3")
-;
-
-CREATE TABLE senders (
-    sender varchar(255) NOT NULL,
-    user varchar(255) NOT NULL,
-    PRIMARY KEY (sender,user)
-);
-
-INSERT INTO senders (sender, user) VALUES
-    ("@example.com","mailer@example.com"),
-    ("@example.net","mailer@example.net"),
-    ("@mail.example.net","mailer@example.net"),
-    ("tenitski@gmail.com","mailer@example.org")
-;
-
-
--- Table structure v2
-
-drop table user;
-drop table sender;
+USE mail;
 
 CREATE TABLE user (
     login varchar(255) NOT NULL,
@@ -57,11 +24,10 @@ INSERT INTO user (login, password) VALUES
     ("mailer@example.org", "password3")
 ;
 
-
 INSERT INTO sender (sender, login) VALUES
     ("@example.com","mailer@example.com"),
     ("@example.net","mailer@example.net"),
     ("@mail.example.net","mailer@example.net"),
-    ("tenitski@gmail.com","mailer@example.org")
+    ("abc@gmail.com","mailer@example.org")
 ;
 
